fccd3d985495358201cf5f187b94609cdf73404b
jeonghyeon Hwang <hijihilin@gmail.com>
2023-07-22 20:04:05 +0900

    [BUILD] Initial Project 추가

---
+++ Mars/Mars/MarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsApp.swift
+//  Mars
+//
+//  Created by 황정현 on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
